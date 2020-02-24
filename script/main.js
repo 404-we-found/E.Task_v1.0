191cf94fc5d8e54029e2ed27561f2d6d5833a02a
CocolikeACha <3605220851@qq.com>
2020-02-25 01:22:47 +0800

    添加了任务创建样式-还未设置字段判断以及请求发送

---
+++ script/main.js
@@ -0,0 +1,14 @@
+import createTask from './taskModule/createtask.js'
+
+
+let welStatus = 1;
+
+if(welStatus) {
+    $(".main-welcome").hide();
+}
+
+
+
+
+
+createTask();
---
+++ script/taskModule/createtask.js
@@ -0,0 +1,39 @@
+var taskName,
+    taskMark,
+    taskBeginTime,
+    taskEndTime;
+
+var createTask = () => {
+    $("#creatTaskBtn").click(() => {
+        $(".main-task-create").addClass("main-task-create-show")
+    });
+
+    $(".main-task").click(function(e){
+        if(e.target.className === "main-task"){
+            $(".main-task-create").removeClass("main-task-create-show")
+        }
+    })
+
+    $("#taskName").blur((e) => {
+        taskName = e.target.value;
+    })
+    $("#taskMark").blur((e) => {
+        console.log(e.target.innerHTML)
+        taskMark = e.target.innerHTML
+    })
+    $("#task_begin_time").blur((e) => {
+        console.log(e.target.value)
+
+        taskBeginTime = e.target.value;
+    })
+    $("#task_end_time").blur((e) => {
+        console.log(e.target.value)
+        taskEndTime = e.target.value;
+    });
+
+    $(".main-task-create-btn").click(()=>{
+        axios.post();
+    })
+}
+
+export default createTask;
